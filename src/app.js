cadc1733e323001f143cbc45fad0b42c51901781
dildurden <dilshankar@gmail.com>
2018-12-06 13:19:28 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+const {accounts,users,writeJSON} = require('./data');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
+
+app.set('views','./src/views');
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,'/public/')));
+app.use(express.urlencoded({extended:true}));
+app.use('/account',accountRoutes);
+app.use('/services',servicesRoutes);
+
+app.get('/',(req,res)=>{
+    res.render('index',{accounts:accounts,title:'Account Summary'});
+});
+
+app.get('/profile',(req,res)=>{
+    res.render('profile',{user:users[0]});
+});
+
+
+app.listen(3000,()=>{console.log('PS Project running on the port 3000!')});
